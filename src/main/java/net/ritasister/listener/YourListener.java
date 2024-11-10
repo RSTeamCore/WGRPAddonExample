7dec84b961c67b1758367b0d02a761d4a50d7868
Rita Wolf <ritasistr@mail.ru>
2024-11-10 02:14:51 +0200

    update api usage

--- src/main/java/net/ritasister/listener/YourListener.java
+++ src/main/java/net/ritasister/listener/YourListener.java
@@ -3,14 +3,13 @@ package net.ritasister.listener;
 import net.kyori.adventure.text.Component;
 import net.ritasister.wgrp.api.WorldGuardRegionProtect;
 import net.ritasister.wgrp.api.WorldGuardRegionProtectProvider;
+import net.ritasister.wgrp.api.manager.regions.RegionAdapterManager;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,10 +21,11 @@ public class YourListener implements Listener {
     @EventHandler
     private void onBreakBlock(@NotNull BlockBreakEvent event) {
         WorldGuardRegionProtect api = WorldGuardRegionProtectProvider.get();
+
         Player player = event.getPlayer();
         Location location = event.getBlock().getLocation();
 
-        if(api.getRegionAdapter().checkStandingRegion(location)) {
+        if(api.getRegionAdapterManager().checkStandingRegion(location)) {
             player.sendMessage(Component.text("You can't break block here"));
             event.setCancelled(true);
         }
@@ -38,7 +38,7 @@ public class YourListener implements Listener {
         Location from = event.getFrom();
         Location to = event.getTo();
 
-        if(api.getRegionAdapter().checkStandingRegion(to) && api.getRegionAdapter().checkStandingRegion(from)) {
+        if(api.getRegionAdapterManager().checkStandingRegion(from) || api.getRegionAdapterManager().checkStandingRegion(to)) {
             player.sendMessage(Component.text("You are joining this region!"));
         } else {
             player.sendMessage(Component.text("You are leave from this region!"));
@@ -49,7 +49,7 @@ public class YourListener implements Listener {
     private void onEntityDamage(@NotNull EntityDamageByEntityEvent event) {
         WorldGuardRegionProtect api = WorldGuardRegionProtectProvider.get();
         if(event.getDamager() instanceof Player damager) {
-            if(api.getRegionAdapter().checkStandingRegion(damager.getLocation())) {
+            if(api.getRegionAdapterManager().checkStandingRegion(damager.getLocation())) {
                 event.setCancelled(true);
                 damager.sendMessage(Component.text("You can't harm anyone!!! :<"));
             }
